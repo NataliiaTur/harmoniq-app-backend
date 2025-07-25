b636985ff9c47ff7360417997a3f5f0031a3c174
StasSkv <stas000123@gmail.com>
2025-07-25 23:56:15 +0300

    articlesRouters ok

--- src/routers/articleRouters.js
+++ src/routers/articleRouters.js
@@ -9,18 +9,30 @@ import {
 } from '../controllers/articleControllers.js';
 import { createArticleSchema } from '../validation/articleValidation.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { isValidId } from '../middlewares/isValidId.js';
+import { isValidArticleId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getArticlesController));
-router.get('/:articleId', isValidId, ctrlWrapper(getArticleByIdController));
+router.get(
+  '/:articleId',
+  isValidArticleId,
+  ctrlWrapper(getArticleByIdController),
+);
 router.post(
   '/',
   validateBody(createArticleSchema),
   ctrlWrapper(createArticleController),
 );
-router.patch('/:articleId', ctrlWrapper(patchArticleController));
-router.delete('/:articleId', ctrlWrapper(deleteArticleController));
+router.patch(
+  '/:articleId',
+  isValidArticleId,
+  ctrlWrapper(patchArticleController),
+);
+router.delete(
+  '/:articleId',
+  isValidArticleId,
+  ctrlWrapper(deleteArticleController),
+);
 
 export default router;

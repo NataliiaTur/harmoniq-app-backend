d3203a1582dd78e302356878ffebd53d684b6fa3
Julia Tarasenko <tarasenkojul88@gmail.com>
2025-07-25 11:34:27 -0700

    Update user

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,29 +1,36 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
     name: {
       type: String,
       default: '',
+      required: true,
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
       unique: true,
       match: [/.+@.+\..+/, 'Email must be valid'],
+      lowercase: true,
     },
     password: {
       type: String,
       required: [true, 'Password is required'],
     },
-    avatarURL: {
+    avatar: {
       type: String,
       default: '',
     },
+    articlesAmount: {
+      type: Number,
+      default: 0,
+    },
     savedArticles: [
       {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'Article',
+        required: true,
       },
     ],
   },
@@ -33,4 +40,11 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-export const User = mongoose.model('User', userSchema);
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+const User = model('User', userSchema);
+export default User;

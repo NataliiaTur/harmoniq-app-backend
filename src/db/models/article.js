f2900deabd20a8f13b4d3311cbb380b7796d5e1f
Nataliia Tur <natalim@ukr.net>
2025-07-25 13:13:46 +0300

    add articleSchema

--- src/db/models/article.js
+++ src/db/models/article.js
@@ -2,50 +2,34 @@ import { Schema, model } from 'mongoose';
 
 const articleSchema = new Schema(
   {
-    id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     img: {
       type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-      minlength: 4,
-      maxlength: 50,
-      trim: true,
     },
     title: {
       type: String,
       required: true,
-      minlength: 3,
-      maxlength: 48,
-      trim: true,
     },
     desc: {
       type: String,
       required: true,
-      minlength: 100,
-      maxlength: 4000,
-      trim: true,
     },
     article: {
       type: String,
       required: true,
-      trim: true,
     },
-    category: {
-      type: String,
-      required: true,
-      enum: ['popular', 'general'],
-    },
-    owner: {
+    ownerId: {
       type: Schema.Types.ObjectId,
       ref: 'User',
       required: true,
+    },
+    rate: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: String,
+      required: true,
+      match: [/^\d{4}-\d{2}-\d{2}$/, 'Дата повинна бути в форматі YYYY-MM-DD'],
     },
   },
   {
